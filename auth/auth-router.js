80052fce82fccde878754f3141d3289e32160e38
Cesare Ferrari <cesare@maryandferrari.com>
2019-08-27 23:18:13 -0400

    Generate a token when user logs in

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const Users = require('../users/users-model.js');
+const secrets = require('../config/secrets.js');
 
 
 // POST /api/register
@@ -27,7 +29,11 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({message: `Welcome ${user.username}`});
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}`,
+          token
+        });
       } else {
         res.status(401).json({message: 'Invalid credentials'});
       }
@@ -36,5 +42,20 @@ router.post('/login', (req, res) => {
       res.status(500).json(err);
     })
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: 'user',
+    username: user.username
+  };
+
+  const secret = secrets.jwtSecret;
+
+  const options = {
+    expiresIn: '1h'
+  }
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
