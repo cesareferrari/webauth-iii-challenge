6050ab4c271e40a2c79c78091ecc7c169da7d471
Cesare Ferrari <cesare@maryandferrari.com>
2019-08-27 22:54:23 -0400

    Register user with hashed password

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const Users = require('../users/users-model.js');
 
 
 // POST /api/register
 router.post('/register', (req, res) => {
   const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
 
   Users.add(user)
     .then(savedUser => {
