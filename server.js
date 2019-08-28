224f6c85c4fc164667bc743d87a51cf6c5c22add
Cesare Ferrari <cesare@maryandferrari.com>
2019-08-27 22:02:22 -0400

    Add server.js file

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const server = express();
+const usersRouter = require('../users/users-router.js');
+const authRouter = require('../auth/auth-router.js');
+
+server.use(express.json());
+
+server.use('/api/auth', authRouter);
+server.use('/api/users', usersRouter);
+
+server.get('/', (req, res) => {
+  res.send('<h1>Welcome to the users management</h1>');
+});
+
+module.exports = server;
